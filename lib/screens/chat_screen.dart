af4d5e2c35fd03b225fc2af12aa46693033c51b0
Lepchaz <ldjigme@gmail.com>
2021-07-30 11:55:36 +0600

    Store Data to Cloud Firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:samuchat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -10,8 +11,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   User? loggedInUser;
+  String? messageText;
 
   @override
   void initState() {
@@ -61,14 +64,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   TextButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      //messageText + loggedInUser.email
+                      _fireStore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser!.email,
+                      });
                     },
                     child: Text(
                       'Send',
